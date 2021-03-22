bc2f80b626dcd10dad9bdf714e17dea601d0f793
Giorgio <giorgio.pignattari@gmail.it>
2021-03-22 18:30:22 +0100

    inizio milestone 1

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+// Milestone 2
+// Coloriamo le icone per tipo.
+// Milestone 3 (BONUS)
+// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+
+$(document).ready(function(){
+
+});
